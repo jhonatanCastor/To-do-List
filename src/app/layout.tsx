4f6cfbf83745d62791ac68b7b126ecf30cc59ea3
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-09 20:07:14 -0300

    hotfix: changed the name bg-slate-500 to black

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-500 text-slate-200`}>{children}</body>
+      <body className={`${inter.className} bg-black text-slate-200`}>{children}</body>
     </html>
   )
 }
